9595718ff8cad7c515729766f2ccbc5c89903a18
Subhransu Bidyasagar Kandi <subhransubidyasagar7@gmail.com>
2024-03-30 14:44:27 +0530

    done

--- src/App.js
+++ src/App.js
@@ -167,7 +167,7 @@ function App() {
     try {
       if (
         imgUrl !== null &&
-        category.length != [] &&
+        category.length !== [] &&
         title !== "" &&
         content !== ""
       ) {
